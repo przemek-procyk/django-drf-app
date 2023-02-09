b9f85ad3e941e318b16d768d89bd40a0dddd66e0
Ruckov <uzytkownik@kursgit.pl>
2023-02-09 17:45:58 +0100

    test

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""
+Django command to wait for the database to be availabe
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
