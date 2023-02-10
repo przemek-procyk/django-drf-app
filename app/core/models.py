6b580ad362dfc43582ffc4c908acc2896378bb16
Ruckov <uzytkownik@kursgit.pl>
2023-02-10 13:47:49 +0100

    typo

--- app/core/models.py
+++ app/core/models.py
@@ -28,6 +28,8 @@ class UserManager(BaseUserManager):
 
         user.save(using=self._db)
 
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
