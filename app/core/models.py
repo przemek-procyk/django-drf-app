b0335d279190b8bb1aa186514d07a10baad9773e
Ruckov <uzytkownik@kursgit.pl>
2023-02-10 13:41:53 +0100

    superuser

--- app/core/models.py
+++ app/core/models.py
@@ -15,8 +15,18 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
+        if not email:
+            raise ValueError('User must have an email address')
+
         return user
 
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
