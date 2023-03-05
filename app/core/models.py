b0a361969341cc9c1b5ad5563237ee37ff89b401
Ruckov <uzytkownik@kursgit.pl>
2023-03-05 11:22:00 +0100

    typo

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ from django.db import models
 
 def recipe_image_file_path(instance, filename):
     """Generate file path for new recipe image"""
-    ext = os.path.splitext(filename[1])
+    ext = os.path.splitext(filename)[1]
     filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads', 'recipe', filename)
